72111bf76deaeacbccdb6f2f335dddde4e14081e
gabriel <gabriel@debian>
2022-10-11 17:10:38 -0300

    Criação da tabela 'VinculosUsuario'

--- banco_de_dados.sql
+++ banco_de_dados.sql
@@ -9,3 +9,5 @@ CREATE TABLE `pds2`.`ConvitesAmizade` (`idConviteAmizade` INT NOT NULL , `fk_con
 CREATE TABLE `pds2`.`Amigos` (`idAmigos` INT NOT NULL , `fk_amigo1` INT NOT NULL , `fk_amigo2` INT NOT NULL , `dataAmizade` DATE NOT NULL , PRIMARY KEY (`idAmigos`)) ENGINE = InnoDB;
 
 CREATE TABLE `pds2`.`AvaliacoesPerfilUsuarios` (`idAvaliacoes` INT NOT NULL , `avaliacoes` TEXT NOT NULL , `fk_avaliador` INT NOT NULL , `fk_avaliado` INT NOT NULL , `score` ENUM('1','2','3','4','5') NOT NULL , PRIMARY KEY (`idAvaliacoes`)) ENGINE = InnoDB;
+
+CREATE TABLE `pds2`.`VinculosUsuario` (`idVinculo` INT NOT NULL , `fk_Usuario` INT NOT NULL , `fk_Vinculo` INT NOT NULL , `dataFimTrabalho` DATE NULL DEFAULT NULL , `dataInicioTrabalho` DATE NOT NULL , `cargo` VARCHAR(50) NOT NULL , PRIMARY KEY (`idVinculo`)) ENGINE = InnoDB;
